f0d7d16826e27b38504c229b9e9f133f0bf1d482
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-04 14:19:37 +0530

    ModifiedCommit

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-import figlet from "figlet";
-
-const nav = document.querySelector("nav");
-const brandName = "";
-
-figlet("Hello World!!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
-
-const h1 = document.createElement("h1");
-h1.append(`${brandName}`);
-nav.insertAdjacentElement("afterbegin", h1);
+const a = document.querySelector("a");
+a.addEventListener("click", function() {
+  window.addEventListener("scroll", function () {
+    a.classList.add('active-state');
+  })
+})
