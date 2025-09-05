128da049c25589811abb2d4d45a70d159c494638
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-05 18:35:22 +0530

    ModCommit

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const cfs = document.querySelectorAll(".container-fluid");
+const cursorDot = document.querySelector(".cursor-dot");
+const cursorOutline = document.querySelector(".cursor-outline");
+
 cfs.forEach((cf) => {
   const hr = document.createElement("hr");
   cf.insertAdjacentElement("afterend", hr);
+})
+
+window.addEventListener("mousemove", (e) => {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, { duration: 300, fill: "forwards"})
 })
